0abb6a98f065eba63c344323330d27e492675b8b
Wellington-gc <wellington_gc@outlook.com>
2022-09-28 01:01:02 -0300

    Implementing main screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import { NextPage } from "next";
+import React from "react";
 
-const Home: NextPage = () => {
-  return <></>;
+const HomePage = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center w-screen h-screen bg-gray-300">
+        <div className="box-content h-96 w-3/4 p-4 border-solid border border-gray-600 rounded-lg bg-gray-700 shadow-xl shadow-slate-500"></div>
+      </div>
+    </>
+  );
 };
 
-export default Home;
+export default HomePage;
