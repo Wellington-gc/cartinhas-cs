4b758b340f1ee3d1bf0ea1c3c38444d3c0e4f8cb
Wellington-gc <wellington_gc@outlook.com>
2022-09-28 00:27:46 -0300

    First patch to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <></>;
+};
+
+export default Home;
